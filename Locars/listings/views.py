967ce6030cc1b78d943f255e666e9f69187a51ae
djamelboubred <djamelboubred.saclay@gmail.com>
2023-10-31 17:57:48 +0100

    add views

--- Locars/listings/views.py
+++ Locars/listings/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
 
-# Create your views here.
+
+
+
+def Home(request: HttpRequest):
+    """
+    The home function renders the home page of the LADN website.
+    
+    
+    :param request: HttpRequest: Pass the request from the server to the function
+    :return: The Home
+    """
+    return render(request, 'listings/Home.html')
